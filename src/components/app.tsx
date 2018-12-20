61a618e91e412cfd28ed76666bb85ea201d95fa4
Vitaly Burlai <vitaly.burlai@gmail.com>
2018-12-20 17:37:42 +0000

    Define function once to avoid re-rendering

--- src/components/app.tsx
+++ src/components/app.tsx
@@ -6,22 +6,22 @@ import DataModel from './data-model';
 import ScrollableArea from './scrollable-area'
 import { API } from './data-model'
 
+const scrollableAreaWrapper = ({ list, loadMore }: API) =>
+  <ScrollableArea
+    list={list}
+    loadMore={loadMore}
+    className={styles.flexible}
+  />
+
 export default function App() {
-    return (
-        <div className={`${styles.container}`}>
-            <h1>Infinite Scroll demo</h1>
-            <ErrorBoundary>
-                <DataModel>
-                    {
-                        ({ list, loadMore }: API) =>
-                            <ScrollableArea
-                                list={list}
-                                loadMore={loadMore}
-                                className={styles.flexible}
-                            />
-                    }
-                </DataModel>
-            </ErrorBoundary>
-        </div>
-    )
+  return (
+    <div className={`${styles.container}`}>
+      <h1>Infinite Scroll demo</h1>
+      <ErrorBoundary>
+        <DataModel>
+          {scrollableAreaWrapper}
+        </DataModel>
+      </ErrorBoundary>
+    </div>
+  )
 }
